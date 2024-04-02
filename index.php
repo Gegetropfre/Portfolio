9e6f893e406b6ec26076db7963da08dd745df9be
Gegetropfre <hete.audric@gmail.com>
2024-04-02 16:41:40 +0200

    :tada: Lancement, page d'acceuil et couleurs définies

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <script src=""></script>
+    <title>Document</title>
+</head>
+<body>
+    <div class="menu">
+        <div class="top">
+            <h1>Audric</h1>
+            <p>Tehkfjdslkqjf</p>
+        </div>
+        <div class="bot">
+            <ul>
+                <li>SHOP<img class="toggle-menu" src='assets/icons/arrow-up-svgrepo-com.svg'> </img></li>
+                <li>ABOUT<img class="toggle-menu" src='assets/icons/arrow-up-svgrepo-com.svg'> </img></li>
+                <li>WORKSHOPS<img class="toggle-menu" src='assets/icons/arrow-up-svgrepo-com.svg'> </img></li>
+                <li>CONNECT <img class="toggle-menu" src='assets/icons/arrow-up-svgrepo-com.svg'> </img></li>
+                <li>THE FACTORY<img class="toggle-menu" src='assets/icons/arrow-up-svgrepo-com.svg'> </img></li>
+            </ul>
+        </div>
+
+    </div>
+    <div class="main">
+        <div class="left">oui</div>
+        <div class="right"></div>
+    </div>
+</body>
+</html>
