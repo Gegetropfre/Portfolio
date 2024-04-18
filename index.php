6a13b30c5210daa8bbf11f8abbfa0d1c34d22585
Gegetropfre <hete.audric@gmail.com>
2024-04-18 23:35:43 +0200

    problème width

--- index.php
+++ index.php
@@ -14,8 +14,14 @@
         <div class="ligne2">
             <div class="left">
                 <h1>Who am I ? <img class="ligne2-exclamation" src="assets\icons\excalamation1-svgrepo-com.svg" alt=""> <img class="ligne2-arrow" src="assets\icons\arrow-hand-drawn.png" alt=""></h1>
+                <h2>Etudiant en développement web</h2>
+                <p>1ère année de BTS SIO option SLAM (Solutions logicielles et applications métier)</p>
             </div>
-            <div class="right"></div>
+            <div class="right">
+            </div>
+        </div>
+        <div class="ligne3">
+            
         </div>
     </div>
 </body>
--- partials/ad.php
+++ partials/ad.php
@@ -1,9 +1,10 @@
 <div class="ad">
-        <h2>Promotion  :  étudiant  gratos  cherche  alternance</h2>
-        <h2>Promotion  :  étudiant  gratos  cherche  alternance    </h2>
-        <h2>Promotion  :  étudiant  gratos  cherche  alternance    </h2>
-        <h2>Promotion  :  étudiant  gratos  cherche  alternance    </h2>
-        <h2>Promotion  :  étudiant  gratos  cherche  alternance    </h2>
-        <h2>Promotion  :  étudiant  gratos  cherche  alternance    </h2>
-        <h2>Promotion  :  étudiant  gratos  cherche  alternance    </h2>
+        <h2>Promotion  :  étudiant  motivé  cherche  alternance  <img src="assets\icons\bolt1-svgrepo-com.svg" alt=""></h2>
+        <h2>Promotion  :  étudiant  motivé  cherche  alternance  <img src="assets\icons\flower1-svgrepo-com.svg" alt=""></h2>
+        <h2>Promotion  :  étudiant  motivé  cherche  alternance  <img src="assets\icons\bubble3-svgrepo-com.svg" alt=""></h2>
+        <h2>Promotion  :  étudiant  motivé  cherche  alternance  <img src="assets\icons\bolt1-svgrepo-com.svg" alt=""></h2>
+        <h2>Promotion  :  étudiant  motivé  cherche  alternance  <img src="assets\icons\flower1-svgrepo-com.svg" alt=""></h2>
+        <h2>Promotion  :  étudiant  motivé  cherche  alternance  <img src="assets\icons\bubble3-svgrepo-com.svg" alt=""></h2>
+        
+        
 </div>
