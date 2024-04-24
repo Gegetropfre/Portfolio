531990ef98b977ec9187e556829c971d09b29991
Gegetropfre <hete.audric@gmail.com>
2024-04-24 15:29:45 +0200

    Page veilles terminée

--- main.js
+++ main.js
@@ -73,18 +73,16 @@ function displayMenu(liId){
         case "li-menu-apropos":
             console.log('display home')
             listeMenu.innerHTML = `
-                <li> Informations </li>
-                <li> CV </li>
-                <li> </li>
-                <li> </li>
+                <li><a href="#">Informations </a></li>
+                <li><a href="contact.php">Informations </a></li>
+                
             `
             break;
-        case "li-menu-contact":
+        case "li-menu-veilles":
             console.log('display contact')
 
             listeMenu.innerHTML = `
-                <li><a href="contact.php">Informations </a></li>
-                <li><a href="contact.php/"> Contacter </a></li>
+                <li><a href="veilles.view.php">Blockchain </a></li>
                 <li> </li>
                 <li> </li>
             `
@@ -106,3 +104,88 @@ function displayMenu(liId){
 
 
 }
+
+
+function changerTitre(){
+    document.title = "Reviens ! " + '\u{1F600}'
+}
+function restaurerTitre(){
+    document.title = "Portoflio - Audric HETE"
+}
+
+window.onblur = changerTitre
+window.onfocus = restaurerTitre
+
+
+// Veilles
+let veilleDivs = document.querySelectorAll('.veilleDiv')
+const veilles = document.querySelector('.veilles')
+const blockchainImg = document.querySelector('#blockchainImg')
+const leftDiv = document.querySelector('.blockchain-left')
+const blockchainh2 = document.querySelector('.blockchainh2')
+
+// creation div description 
+const blockchainDesc = document.createElement('div')
+blockchainDesc.classList.add('blockchain-desc')
+blockchainDesc.textContent = 'OII'
+body.appendChild(blockchainDesc)
+
+
+function stopAnimation() {
+    // Récupérer l'angle de rotation actuel
+    const computedStyle = window.getComputedStyle(blockchainImg);
+    const currentRotation = computedStyle.getPropertyValue('transform') || 'none';
+    
+    // Supprimer la classe pour arrêter l'animation
+    blockchainImg.classList.remove('imgBlockchainActive');
+    
+    // Appliquer l'angle de rotation actuel directement à l'image
+    blockchainImg.style.transform = currentRotation;
+}
+
+// Animation veilles
+
+    
+
+
+function handleMouseOver(){
+        leftDiv.classList.add('leftActive')
+        blockchainImg.classList.add('imgBlockchainActive')
+        blockchainh2.classList.add('blockchainh2Active')
+    }
+
+function handleMouseOut(){
+    leftDiv.classList.remove('leftActive')
+    blockchainh2.classList.remove('blockchainh2Active')
+
+    stopAnimation()
+}
+    
+    setTimeout(()=>{
+        veilles.addEventListener('mouseover', handleMouseOver)
+        blockchainDesc.addEventListener('mouseover', handleMouseOver)
+        veilles.addEventListener('mouseout', handleMouseOut)
+        blockchainDesc.addEventListener('mouseout', handleMouseOut)
+
+    }, 1000)
+
+    
+
+// Redirection boutons
+const paths = {0 : 'https://www.cointribune.com/blockchain-spacechain-et-velas-font-dans-la-grande-vitesse/',
+               1: 'https://www.lebigdata.fr/cybersecurite-blockchain',
+               2: 'https://geodis.com/fr/blog/blockchain-la-logistique-innove-dans-le-transport-des-vaccins'
+                 }
+
+const articleBtns = document.querySelectorAll('.veilles-btns');
+
+articleBtns.forEach((articleBtn, index) => {
+    articleBtn.addEventListener('click', ()=>{
+
+        console.log(`Index: ${index}, Bouton:`, articleBtn);
+        window.location.href = paths[index]
+    })
+});
+
+
+               
---
+++ veilles.view.php
@@ -0,0 +1,58 @@
+<?php include 'partials/head.php' ?>
+<link rel="stylesheet" href="style.css">
+<link rel="stylesheet" href="style-veilles.css">
+
+
+<body>
+    <?php include 'partials/ad.php' ?>
+    <?php include 'partials/header.php' ?>
+
+    <div class="veilles">
+        <div class="blockchain-left" >
+            <h2 class ="blockchainh2">BLOCKCHAIN</h2>
+            <img id = "blockchainImg" src="assets\icons\BlockchainImg.png" alt="">
+            
+        </div>
+        <div class="blockchain-middle-left veilleDiv">
+            <div class="blockchain">
+                <img src="assets\icons\space.jpg" class ="rocketImg" alt="">
+                <p>Bitcoin & FTT : Étude de la blockchain suite à la faillite FTX </p>
+                <button class="veilles-btns" href="https://www.cointribune.com/blockchain-spacechain-et-velas-font-dans-la-">
+                    <p>Article</p>
+                    <img src="assets\icons\article.svg" class="article-img" alt="">
+                </button>
+
+            </div>
+        </div>
+        <div class="blockchain-middle-right veilleDiv">
+            <div class="blockchain">
+            <img src="assets/icons/Affaire_FTX.webp" class ="rocketImg" alt="">
+                <p>Bitcoin & FTT : Étude de la blockchain suite à la faillite FTX </p>
+                <button class="veilles-btns" href="https://www.cointribune.com/blockchain-spacechain-et-velas-font-dans-la-">
+                    <p>Article</p>
+                    <img src="assets\icons\article.svg" class="article-img" alt="">
+                </button>
+            </div>
+        </div>
+        <div class="blockchain-right veilleDiv">
+            <div class="blockchain">
+            <img src="assets\icons\La_logistique_innove.webp" class ="rocketImg" alt="">
+                <p>Bitcoin & FTT : Étude de la blockchain suite à la faillite FTX </p>
+                <button class="veilles-btns" href="https://www.cointribune.com/blockchain-spacechain-et-velas-font-dans-la-">
+                    <p>Article</p>
+                    <img src="assets\icons\article.svg" class="article-img" alt="">
+                </button>
+            </div>
+        </div>
+    </div>
+
+
+
+    
+</body>
+
+
+
+
+
+
